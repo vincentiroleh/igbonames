f73e93c8656a3b7ae653de06cbb41d8210679295
Vincent Iroleh <irolehiroleh@gmail.com>
2021-05-24 11:41:33 +0100

    chore(Add API versioning)

--- app/index.js
+++ app/index.js
@@ -5,16 +5,19 @@ router.get('/', (req, res) => {
     res.json({
         message: 'Welcome to Igbo Names API, here you will find Igbo names with their pet name and meaning.',
         API_resources: [
-            "/boys", "/boys/name", "/girls", "/girls/name", 
+            "/boys", "/boys/name", "/girls", "/girls/name",
         ]
     })
 })
 
+const apiV1 = '/api/v1';
+
 // Boys route
-router.get('/boys', Controller.getAllBoys);
-router.get('/boys/:name', Controller.getBoyName);
+router.get(`${apiV1}/boys`, Controller.getAllBoys);
+router.get(`${apiV1}/boys/:name`, Controller.getBoyName);
 
 // Girls Route
-router.get('/girls', Controller.getAllGirls);
-router.get('/girls/:name', Controller.getGirlName);
+router.get(`${apiV1}/girls`, Controller.getAllGirls);
+router.get(`${apiV1}/girls/:name`, Controller.getGirlName);
+
 module.exports = router;
